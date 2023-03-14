f181a7fe41f6cc61f94029af3fc627e15978adaa
volodymyr35 <vzhukevych@gmail.com>
2023-03-14 23:55:50 +0200

    added some changes to styling files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,6 @@ import cart from "../../images/cart.svg";
 import avatar from "../../images/avatar.png";
 import books from "../../images/book.png";
 
-
 import "./Header.css";
 
 export function Header({ username, setUsername }) {
@@ -26,25 +25,23 @@ export function Header({ username, setUsername }) {
   }
   return (
     <div className="header">
-      <div className="header__container">
-        <h2>X-course task / {username}</h2>
-        <nav className="navigation">
-          <Link to="/books">
-            <img src={books} alt="books" width="40em" />
-          </Link>
+      <h2>X-course task / {username}</h2>
+      <nav className="navigation">
+        <Link to="/books">
+          <img src={books} alt="books" width="40em" />
+        </Link>
 
-          <CartCounter>
-            <Link to="/cart">
-              <img src={cart} alt="cart" width="40em" />
-            </Link>
-          </CartCounter>
-          <button className="sign-out" onClick={navigateToSignIn}>
-            Sign-Out
-          </button>
-          <img src={avatar} alt="user avatar" width="40em" />
-          <span>{username}</span>
-        </nav>
-      </div>
+        <CartCounter>
+          <Link to="/cart">
+            <img src={cart} alt="cart" width="40em" />
+          </Link>
+        </CartCounter>
+        <button className="sign-out" onClick={navigateToSignIn}>
+          Sign-Out
+        </button>
+        <img src={avatar} alt="user avatar" width="40em" />
+        <span>{username}</span>
+      </nav>
     </div>
   );
 }
